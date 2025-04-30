36f78cf66f986f5ef648cc9a4d92885df675750f
Manal-0 <manalaldeweesh@gmail.com>
2025-04-30 13:20:15 +0300

    Add initial project files

--- nginx-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-yum update -y 
-yum install nginx -y 
-systemctl start nginx 
-systemctl enable nginx 
-aws s3 cp s3://sda2020manal-clarusway-assets/index.html 
-/usr/share/nginx/html/index.html
